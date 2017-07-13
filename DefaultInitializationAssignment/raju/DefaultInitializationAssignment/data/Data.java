65aff53b79c5f9e1e31c2727e5ca9848c897e758
Bhupathi-Raju <bhupathi.raju@zemosolabs.com>
2017-07-13 19:58:14 +0530

    Assignment resubmission

--- DefaultInitializationAssignment/raju/DefaultInitializationAssignment/data/Data.java
+++ DefaultInitializationAssignment/raju/DefaultInitializationAssignment/data/Data.java
@@ -4,8 +4,8 @@ package raju.DefaultInitializationAssignment.data;
  * Created by zemoso on 29/6/17.
  */
 public class Data {
-    private int a;
-    private char b;
+    private int a;  // This is a class field which gets initialised automatically
+    private char b; // This is a class field which gets initialised automatically
     public void print()
     {
         System.out.println(a);
@@ -13,7 +13,7 @@ public class Data {
     }
     public void print2()
     {
-         int c,d;
+         int c,d;  // These are method fields which does't get initialised automatically
          /*System.out.println(c);    //This code doesn't execute because fields c & d were not initialised
          System.out.println(d);*/     // Fields inside a class gets initialised automatically but not fields
                                        // fields in the methods.
--- DefaultInitializationAssignment/raju/DefaultInitializationAssignment/singleton/Singleton.java
+++ DefaultInitializationAssignment/raju/DefaultInitializationAssignment/singleton/Singleton.java
@@ -4,12 +4,13 @@ package raju.DefaultInitializationAssignment.singleton;
  * Created by zemoso on 29/6/17.
  */
 public class Singleton {
-     private Singleton(){}
+     private Singleton(){}  // as a singleton class should have only one object we make it's constructor private
+     private static Singleton obj1= new Singleton(); // and its object static
      private String s;
      public static Singleton getString(String str)
      {
-         Singleton obj1= new Singleton();   // if object of the class is not created, we cannot access Non-static fields from
-          //Static fields.  
+         // if object of the class is not created, we cannot access Non-static fields from
+         //Static fields.
          obj1.s = str;
          return obj1;
      }
