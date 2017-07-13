eba75e11fe6372ab55862333f20b00bf60021f94
Bhupathi-Raju <bhupathi.raju@zemosolabs.com>
2017-07-13 19:02:36 +0530

    WordCount Assignment

--- WordCount/src/WordCount.java
+++ WordCount/src/WordCount.java
@@ -5,56 +5,41 @@ import java.util.Map;
 /**
  * Created by zemoso on 7/7/17.
  */
-///This is to be run from command line
 public class WordCount {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Map<Character, Integer> m = new HashMap<>();
-        BufferedReader inputStream = null;
-        BufferedWriter outputStream = null;
+        BufferedReader inputStream;
        try {
-           Console q = System.console();
-           if (q == null) {
-               System.err.println("No console.");
-               System.exit(0);
-           }
-           String inputFileName = q.readLine("Enter the  input file name: ");
-           String outputFileName = q.readLine("Enter the output file name: ");
-           File a = new File(inputFileName);
-           File b = new File(outputFileName);
-           inputStream = new BufferedReader(new FileReader(a));
-           outputStream = new BufferedWriter(new FileWriter(b));
-           int c;
-           while((c=inputStream.read())!=-1)
-           {
-               int value;
-               char ch = (char)c;
-                if(m.containsKey(ch))
-                {
-                     value = m.get(ch)+1;
-                }
-                else
-                     value=1;
-                m.put(ch,value);
-           }
-           for(Map.Entry<Character,Integer> entry : m.entrySet())
-           {
-               char c1 = entry.getKey();
-               int d = entry.getValue();
-               System.out.println(d);
-               if(c1 =='\n')
-                   outputStream.write("New Line:");
-               else
-               outputStream.write(c1 );
-               outputStream.write(" "+"=");
-               outputStream.write(String.valueOf(d));
-               outputStream.newLine();
-           }
-       }
-       finally{
-            if(inputStream!=null)
-                inputStream.close();
-            if(outputStream!=null)
-                outputStream.close();
-       }
+            String inputFileName = args[0];  //This takes input from the command line arguments as the input file name
+            String outputFileName = args[1];  // taking input form the command line arguments as output file name
+            PrintWriter writer = new PrintWriter(outputFileName);
+            File a = new File(inputFileName);
+            inputStream = new BufferedReader(new FileReader(a));
+            int c;
+            // Mapping Characters with keys,Here keys are the values how many times they are repeated.
+            while ((c = inputStream.read()) != -1) {
+                int value;
+                char ch = (char) c;
+                if (m.containsKey(ch)) {
+                    value = m.get(ch) + 1;
+                } else value = 1;
+                m.put(ch, value);
+            }
+            //writing out the output to the file "output.txt" by iterating through the entire map
+            for (Map.Entry<Character, Integer> entry : m.entrySet()) {
+                char c1 = entry.getKey();
+                int d = entry.getValue();
+                if (c1 == '\n') writer.write("New Line:");
+                else writer.write(c1);
+                writer.write(" " + "=");
+                writer.println(String.valueOf(d));
+            }
+               writer.close();
+               inputStream.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
